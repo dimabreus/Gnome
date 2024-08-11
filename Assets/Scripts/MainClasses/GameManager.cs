641de0e94f9b232912910a28bd9c27be6850920a
dimabreus <dmitriybreus5@gmail.com>
2024-08-11 15:36:52 +0300

    Refactored GameManager and fixed set max fps

--- Assets/Scripts/MainClasses/GameManager.cs
+++ Assets/Scripts/MainClasses/GameManager.cs
@@ -1,31 +1,38 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private Slider _sliderFPS;
-    [SerializeField] private TMPro.TextMeshProUGUI _fpsText;
+    [SerializeField] private Slider _fpsSlider;
+    [SerializeField] private TextMeshProUGUI _fpsText;
+    [SerializeField] private int _fpsMultiplier = 30;
 
-	private void Start()
-	{
-		SetFPS();
-	}
-
-	private void Update()
-	{
-		_fpsText.text = "FPS: " + ((int)(1 / Time.unscaledDeltaTime)).ToString();
-	}
-
-	public void SetFPS()
+    private void Start()
     {
-		if (_sliderFPS.value < 0) return;
+        SetFPS();
+    }
 
-		Application.targetFrameRate = (int)((_sliderFPS.value + 1) * 30);
-		QualitySettings.vSyncCount = 0;
-	}
+    private void Update()
+    {
+        float fps = 1 / Time.unscaledDeltaTime;
+        _fpsText.text = "FPS: " + Mathf.RoundToInt(fps).ToString();
+    }
 
-	public void CloseGame()
-	{
-		Application.Quit();
-	}
+    public void SetFPS()
+    {
+        if (_fpsSlider.value < 0f)
+        {
+            Application.targetFrameRate = Mathf.RoundToInt(Mathf.Infinity);
+            QualitySettings.vSyncCount = 1;
+        };
+
+        Application.targetFrameRate = Mathf.RoundToInt(_fpsSlider.value + 1) * _fpsMultiplier;
+        QualitySettings.vSyncCount = 0;
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
 }
