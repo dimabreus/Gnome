5987d5e021256763c5074825a5e1ecffc92a7575
eAnd <andrejmisurin1@gmail.com>
2024-08-11 13:22:47 +0300

    Added lock fps

--- Assets/Scripts/MainClasses/GameManager.cs
+++ Assets/Scripts/MainClasses/GameManager.cs
@@ -6,6 +6,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Slider _sliderFPS;
     [SerializeField] private TMPro.TextMeshProUGUI _fpsText;
 
+	private void Start()
+	{
+		SetFPS();
+	}
+
 	private void Update()
 	{
 		_fpsText.text = "FPS: " + ((int)(1 / Time.unscaledDeltaTime)).ToString();
@@ -13,8 +18,11 @@ public class GameManager : MonoBehaviour
 
 	public void SetFPS()
     {
-        Application.targetFrameRate = (int)(_sliderFPS.value * 30) >= 3 ? 1000 : (int)(_sliderFPS.value * 30);
-    }
+		if (_sliderFPS.value < 0) return;
+
+		Application.targetFrameRate = (int)((_sliderFPS.value + 1) * 30);
+		QualitySettings.vSyncCount = 0;
+	}
 
 	public void CloseGame()
 	{
